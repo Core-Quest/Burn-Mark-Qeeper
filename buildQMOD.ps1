3823c1322a19dac2bec76002a40bd64ef5249fc4
Core <83958883+Core-Quest@users.noreply.github.com>
2021-09-18 16:10:46 -0400

    fixed hook version

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -10,5 +10,5 @@ $ArchiveName = "burnmarkqeeper_v0.1.0.qmod"
 $TempArchiveName = "burnmarkqeeper_v0.1.0.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libburnmarkqeeper.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libburnmarkqeeper.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_1.so", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
